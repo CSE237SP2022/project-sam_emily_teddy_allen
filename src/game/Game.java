b9bf6a1a8cb2f658acf86e97b3ac8c8bf7c92a09
tfujimotowustl <59811628+tfujimotowustl@users.noreply.github.com>
2022-03-27 18:03:55 -0500

    initial character commit

--- src/game/Game.java
+++ src/game/Game.java
@@ -1,18 +1,26 @@
 package game;
+import java.util.Scanner;
 
 public class Game {
 	
-	public void createCharacter() {
-		
+	public static void createCharacter() {
+		Scanner scan = new Scanner(System.in);
+	    System.out.println("What is your name?");
+		String name = scan.nextLine();
+		Player player= new Player(name);
+		System.out.println("Hello "+ player.getName()+" please enter anything to continue");
+		scan.nextLine();
+		scan.close();
+		System.out.println("Starting game");
 	}
 	
 	
-	public void intro() {
+	public static void intro() {
 		createCharacter();
 	}
 	
 	public static void main(String[] args) {
 		
-		
+		intro();
 	}
 }
---
+++ src/game/Player.java
@@ -0,0 +1,31 @@
+package game;
+import java.util.LinkedList; //gonna add magic to this
+
+public class Player {
+	private String name;
+	private int level;
+	private int attackStat;
+	private double health;
+	
+
+	public Player(String playerName) {
+		// TODO Auto-generated constructor stub
+		this.name=playerName;
+		this.level=1;
+		this.attackStat=5;
+		this.health=100;
+	}
+	public String getName() {
+		return name;
+	}
+	public int getLevel() {
+		return level;
+	}
+	public double getHealth() {
+		return health;
+	}
+	public int getAttackStat() {
+		return attackStat;
+	}
+
+}
