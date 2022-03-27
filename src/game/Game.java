73bd63acdd1328158e2ac89b989a0dc862f2a253
tfujimotowustl <59811628+tfujimotowustl@users.noreply.github.com>
2022-03-26 22:02:39 -0500

    intro commit

---
+++ src/game/Game.java
@@ -0,0 +1,18 @@
+package game;
+
+public class Game {
+	
+	public void createCharacter() {
+		
+	}
+	
+	
+	public void intro() {
+		createCharacter();
+	}
+	
+	public static void main(String[] args) {
+		
+		
+	}
+}
