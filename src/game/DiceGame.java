39b5408fc95946a859e6ac28bac3c4aeda67b3f5
tfujimotowustl <59811628+tfujimotowustl@users.noreply.github.com>
2022-03-28 19:53:03 -0500

    initial commit for dice game

---
+++ src/game/DiceGame.java
@@ -0,0 +1,14 @@
+package game;
+
+public class DiceGame {
+
+	public DiceGame() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public static void main(String[] args) {
+		System.out.println("Test");
+
+	}
+
+}
