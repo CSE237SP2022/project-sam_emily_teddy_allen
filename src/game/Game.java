aedd83d00a22e6ecac18c2f36b5e75733b0c21a8
Allen Buckner <abuckner@wustl.edu>
2022-04-12 01:14:51 -0500

    Updated story loop to use functions at each step for improved modularity

--- src/game/Game.java
+++ src/game/Game.java
@@ -26,6 +26,190 @@ public class Game {
 		
 	}
 	
+	public static int start() {
+		System.out.println("You begin your journey in the woods. Trees extend in every direction. "
+				+ "Upon closer inspection you notice two trails that seemed to be stamped out "
+				+ "across the ground. Do you choose the path to the left or the right?");
+		System.out.println("Left (Enter 'L')");
+		System.out.println("Right (Enter 'R')");
+		int progress = 0;
+		String choice = scan.nextLine();
+		while(progress == 0)
+		{
+			if(choice.equals("L"))
+			{
+				return 1;
+			}
+			else if(choice.equals("R"))
+			{
+				return 2;
+			}
+			else
+			{
+				System.out.println("Please Enter 'L' or 'R'");
+				choice = scan.nextLine();
+			}
+		}
+		return 0;
+	}
+	
+	public static int hooded() {
+		
+		System.out.println("You head down the path and notice a hooded figure "
+				+ "in the distance. As the hooded figure becomes more defined, you notice they're "
+				+ "facing your direction and their hand is out. Within it, you view two dice and "
+				+ "suddenly the figure utters in a high, nasally voice 'Wanna play with my dice'?");
+		System.out.println("");
+		System.out.println("Yes (Enter 'Y')");
+		System.out.println("");
+		System.out.println("No (Enter 'N')");
+		System.out.println("");
+		int progress = 0;
+		
+		String choice = scan.nextLine();
+		while(progress == 0)
+		{
+			if(choice.equals("Y"))
+			{
+				return 3;
+			}
+			else if(choice.equals("N"))
+			{
+				return 4;
+			}
+			else
+			{
+				System.out.println("Please Enter 'Y' or 'N'");
+				System.out.println("");
+				choice = scan.nextLine();
+			}
+		}
+		return 0;
+	}
+	
+	public static int elf() {
+		
+		System.out.println("You head down the path. Your hear ruffling in the trees "
+				+ "above you. As you look up, an elf-like creature falls down in front of you. "
+				+ "'BET YOU CAN'T BEAT ME IN CARDS' he shouts. ");
+		System.out.println("");
+		System.out.println("'YES I CAN!' (Enter 'Y')");
+		System.out.println("");
+		System.out.println("Run away (Enter 'N')");
+		System.out.println("");
+		int progress = 0;
+		
+		String choice = scan.nextLine();
+		while(progress == 2)
+		{
+			if(choice.equals("Y"))
+			{
+				return 5;
+			}
+			else if(choice.equals("N"))
+			{
+				System.out.println("You run back to the left path. ");
+				System.out.println("");
+				return 1;
+			}
+			else
+			{
+				System.out.println("Please Enter 'Y' or 'N'");
+				System.out.println("");
+				choice = scan.nextLine();
+			}
+		}
+		return 0;
+		
+	}
+	
+	public static int dice() {
+		DiceGame game = new DiceGame();
+		boolean win = game.play();
+		if(win)
+		{
+			return 7;
+		}
+		else
+		{
+			System.out.println("You have lost. You glance towards the hooded figure in "
+					+ "defeat.");
+			System.out.println("");
+			return 4;
+		}
+	}
+	
+	public static int combat() {
+		System.out.println("They suddenly morph into a monster and attack you!");
+		System.out.println("");
+		//combat sequence
+		
+		Player enemy = new Player("Monster");
+		Battle battle = new Battle(player1, enemy);
+		int endCon = battle.fight();
+		if(endCon == 0) // Enemy died
+		{
+			return 7;
+		}
+		else if(endCon == 1) // You ran
+		{
+			return 9;
+		}
+		else if(endCon == 2) // Enemy ran
+		{
+			return 2;
+		}
+		else // You died
+		{
+			System.out.println("You were slain by your foe. Unfortunately, your journey "
+					+ "stops here...");
+			System.out.println("");
+			return 10;
+		}
+		
+	}
+	
+	public static int cards() {
+		//card game
+		String [] cards = new String[10];
+		Blackjack.main(cards);
+		
+		return 6;
+	}
+	
+	public static int happy_elf() {
+		System.out.println("After finishing the game, your new friend seems pleased. "
+				+ "'Thank you for playing with me!', he grins and gestures for you to "
+				+ "follow him. He leads you to a path that seems to lead out of the forest. "
+				+ "'Right this way and you'll be free!'");
+		System.out.println("");
+		return 8;
+	}
+	
+	public static int defeat_enemy() {
+		System.out.println("Upon defeat, the enemy screeches in pain and fades "
+				+ "away into a dark mist. The mist blows through the tress in front of "
+				+ "you revealing a path out of the forest.");
+		System.out.println("");
+		return 8;
+	}
+	
+	public static int win() {
+		System.out.println("After following the path, you "
+				+ "are relieved to see you are home. Your journey is now complete. You Win!");
+		System.out.println("");
+		return 10;
+	}
+	
+	public static int lost() {
+		System.out.println("You ran as fast as you could, losing sight of the things around you. "
+				+ "After a few minutes, you realize you're no longer able to find the paths that "
+				+ "were once in front of you. With no sense of direction you wander the forest, "
+				+ "lost, for all eternity...");
+		System.out.println("");
+		return 10;
+	}
+	
 	public static void story() {
 		
 		int progress = 0;
@@ -34,185 +218,47 @@ public class Game {
 		{
 			if(progress == 0) //Start
 			{
-				System.out.println("You begin your journey in the woods. Trees extend in every direction. "
-						+ "Upon closer inspection you notice two trails that seemed to be stamped out "
-						+ "across the ground. Do you choose the path to the left or the right?");
-				System.out.println("Left (Enter 'L')");
-				System.out.println("Right (Enter 'R')");
-				
-				String choice = scan.nextLine();
-				while(progress == 0)
-				{
-					if(choice.equals("L"))
-					{
-						progress = 1;
-					}
-					else if(choice.equals("R"))
-					{
-						progress = 2;
-					}
-					else
-					{
-						System.out.println("Please Enter 'L' or 'R'");
-						choice = scan.nextLine();
-					}
-				}
-				
+				progress = start();
 			}
 			if(progress == 1) //Hooded Figure
 			{
-				System.out.println("You head down the path and notice a hooded figure "
-						+ "in the distance. As the hooded figure becomes more defined, you notice they're "
-						+ "facing your direction and their hand is out. Within it, you view two dice and "
-						+ "suddenly the figure utters in a high, nasally voice 'Wanna play with my dice'?");
-				System.out.println("");
-				System.out.println("Yes (Enter 'Y')");
-				System.out.println("");
-				System.out.println("No (Enter 'N')");
-				System.out.println("");
-				
-				String choice = scan.nextLine();
-				while(progress == 1)
-				{
-					if(choice.equals("Y"))
-					{
-						progress = 3;
-					}
-					else if(choice.equals("N"))
-					{
-						progress = 4;
-					}
-					else
-					{
-						System.out.println("Please Enter 'Y' or 'N'");
-						System.out.println("");
-						choice = scan.nextLine();
-					}
-				}
+				progress = hooded();
 			}
 			
 			if(progress == 2) //Elf
 			{
-				System.out.println("You head down the path. Your hear ruffling in the trees "
-						+ "above you. As you look up, an elf-like creature falls down in front of you. "
-						+ "'BET YOU CAN'T BEAT ME IN CARDS' he shouts. ");
-				System.out.println("");
-				System.out.println("'YES I CAN!' (Enter 'Y')");
-				System.out.println("");
-				System.out.println("Run away (Enter 'N')");
-				System.out.println("");
-				
-				String choice = scan.nextLine();
-				while(progress == 2)
-				{
-					if(choice.equals("Y"))
-					{
-						progress = 5;
-					}
-					else if(choice.equals("N"))
-					{
-						System.out.println("You run back to the left path. ");
-						System.out.println("");
-						progress = 1;
-					}
-					else
-					{
-						System.out.println("Please Enter 'Y' or 'N'");
-						System.out.println("");
-						choice = scan.nextLine();
-					}
-				}
-				
+				progress =  elf();
 			}
 			
 			if(progress == 3) //Dice
 			{
-				DiceGame game = new DiceGame();
-				boolean win = game.play();
-				if(win)
-				{
-					progress = 7;
-				}
-				else
-				{
-					System.out.println("You have lost. You glance towards the hooded figure in "
-							+ "defeat.");
-					System.out.println("");
-					progress = 4;
-				}
+				progress = dice();
 			}
 			
 			if(progress == 4) //Combat
 			{
-				System.out.println("They suddenly morph into a monster and attack you!");
-				System.out.println("");
-				//combat sequence
-				
-				Player enemy = new Player("Monster");
-				Battle battle = new Battle(player1, enemy);
-				int endCon = battle.fight();
-				if(endCon == 0) // Enemy died
-				{
-					progress = 7;
-				}
-				else if(endCon == 1) // You ran
-				{
-					progress = 9;
-				}
-				else if(endCon == 2) // Enemy ran
-				{
-					progress = 2;
-				}
-				else // You died
-				{
-					System.out.println("You were slain by your foe. Unfortunately, your journey "
-							+ "stops here...");
-					System.out.println("");
-					progress = 10;
-				}
-				
+				progress = combat();
 			}
 			
 			if(progress == 5) //Cards
 			{
-				//card game
-				String [] cards = new String[10];
-				Blackjack.main(cards);
-				
-				progress = 6;
+				progress = cards();
 			}
 			if(progress == 6) //Happy Elf
 			{
-				System.out.println("After finishing the game, your new friend seems pleased. "
-						+ "'Thank you for playing with me!', he grins and gestures for you to "
-						+ "follow him. He leads you to a path that seems to lead out of the forest. "
-						+ "'Right this way and you'll be free!'");
-				System.out.println("");
-				progress = 8;
+				progress = happy_elf();
 			}
 			if(progress == 7) //Defeat Enemy
 			{
-				System.out.println("Upon defeat, the enemy screeches in pain and fades "
-						+ "away into a dark mist. The mist blows through the tress in front of "
-						+ "you revealing a path out of the forest.");
-				System.out.println("");
-				progress = 8;
+				progress = defeat_enemy();
 			}
 			if(progress == 8) //Win
 			{
-				System.out.println("After following the path, you "
-						+ "are relieved to see you are home. Your journey is now complete. You Win!");
-				System.out.println("");
-				progress = 10;
+				progress = win();
 			}
 			if(progress == 9) //Lost
 			{
-				System.out.println("You ran as fast as you could, losing sight of the things around you. "
-						+ "After a few minutes, you realize you're no longer able to find the paths that "
-						+ "were once in front of you. With no sense of direction you wander the forest, "
-						+ "lost, for all eternity...");
-				System.out.println("");
-				progress = 10;
+				progress = lost();
 			}
 		}
 		
@@ -221,10 +267,7 @@ public class Game {
 	}
 	
 	public static void main(String[] args) {
-		
 		intro();
 		story();
-		
-		//combat();
 	}
 }
