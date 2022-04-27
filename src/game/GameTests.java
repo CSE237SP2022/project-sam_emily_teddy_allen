cd7073fee38da13d05f5e7e2997c9717a0473403
Allen Buckner <abuckner@wustl.edu>
2022-04-27 08:37:58 -0500

    Added additional tests for Game class

--- src/game/GameTests.java
+++ src/game/GameTests.java
@@ -1,4 +1,6 @@
 package game;
+import java.util.Scanner;
+
 
 import static org.junit.Assert.assertTrue;
 import static org.junit.jupiter.api.Assertions.*;
@@ -16,12 +18,56 @@ class GameTests {
 	@Test
 	void PlayerTest() {
 		Game game = new Game();
-		assertNull(game.getPlayer());
+		Player test = game.getPlayer();
+		assertNull(test);
 	}
 	
 	@Test
 	void ProgressTest() {
 		Game game = new Game();
-		assertTrue(game.getProgress() == 0);
+		int test = game.getProgress();
+		assertEquals(0, test);
+	}
+	
+	void ScannerTest() {
+		Game game = new Game();
+		Scanner test = game.getScanner();
+		assertNotNull(test);
+	}
+	
+	void HappyElfTest()
+	{
+		Game game = new Game();
+		int test = game.happy_elf();
+		assertEquals(10, test);
+	}
+	
+	void DefeatTest()
+	{
+		Game game = new Game();
+		int test = game.defeat_enemy();
+		assertEquals(10, test);
+	}
+	
+	void WinTest()
+	{
+		Game game = new Game();
+		int test = game.win();
+		assertEquals(12, test);
+	}
+	
+	void LostTest()
+	{
+		Game game = new Game();
+		int test = game.lost();
+		assertEquals(12, test);
+	}
+	
+	void StoreTest()
+	{
+		Game game = new Game();
+		int test = game.store();
+		assertEquals(11, test);
+		
 	}
 }
